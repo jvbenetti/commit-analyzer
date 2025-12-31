db429a1c85de74e984436767082ca94ad56577a8
JaoVicy <filipimjoa@gmail.com>
2025-12-31 16:00:14 -0300

    style: add comments

--- internal/analyzer/CommitAn.go
+++ internal/analyzer/CommitAn.go
@@ -5,11 +5,11 @@ import (
 )
 
 func CommitAn(message string) string {
-	part := strings.SplitN(message, ":", 2)
+	part := strings.SplitN(message, ":", 2) // Divide the string in first ":"
 
 	if len(part) < 2 {
 		return "Another's commits"
 	}
-
+	// Return the first part, clean without additional spaces
 	return strings.ToLower(strings.TrimSpace(part[0]))
 }
