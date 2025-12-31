61a9e3d9c071dd2a5468273b59e4e905613ffb90
JaoVicy <filipimjoa@gmail.com>
2025-12-31 20:42:00 -0300

    feat: defined vars for route and error catch

--- cmd/setup/main.go
+++ cmd/setup/main.go
@@ -4,3 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"setup/internal/provider"
 )
+
+func GetCommits(c *gin.Context) {
+	user := c.Param("user")
+	repo := c.Param("repo")
+	commits, err := provider.SearchCommits(user, repo)
+	if err != nil {
+		return
+	}
+}
