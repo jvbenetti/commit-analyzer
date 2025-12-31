e47254768c5bc033912acc7208e6db3ae4e0d6e1
JaoVicy <filipimjoa@gmail.com>
2025-12-31 15:57:28 -0300

    feat: created CommitAn func

--- internal/analyzer/CommitAn.go
+++ internal/analyzer/CommitAn.go
@@ -1,1 +1,15 @@
 package analyzer
+
+import (
+	"strings"
+)
+
+func CommitAn(message string) string {
+	part := strings.SplitN(message, ":", 2)
+
+	if len(part) < 2 {
+		return "Another's commits"
+	}
+
+	return strings.ToLower(strings.TrimSpace(part[0]))
+}
