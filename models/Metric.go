0d9d9d8b5a08ce34bca2a642ceaba54fe405a7d8
JaoVicy <filipimjoa@gmail.com>
2026-01-07 20:47:04 -0300

    style: add comment for Type string

--- models/Metric.go
+++ models/Metric.go
@@ -2,5 +2,5 @@ package models
 
 type Metric struct {
 	Total int            `json:"total"` // Total of commits
-	Type  map[string]int `json:"type"`
+	Type  map[string]int `json:"type"`  // Get string with its number of letters
 }
