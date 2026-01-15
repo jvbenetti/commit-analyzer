35d51fbb4bb7721fee276881486bf0c6a4079f79
JaoVicy <filipimjoa@gmail.com>
2026-01-15 18:27:24 -0300

    feat: add Init() method in model

--- models/BubbleTea.go
+++ models/BubbleTea.go
@@ -1,7 +1,14 @@
 package models
 
+import tea "github.com/charmbracelet/bubbletea"
+
 type BubbleTea struct {
 	Choices  []string         // items on the to-do list
 	Cursor   int              // which to-do list item our cursor is pointing at
 	Selected map[int]struct{} // which to-do items are selected
+}
+
+func (m BubbleTea) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
 }
