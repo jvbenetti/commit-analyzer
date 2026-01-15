3240699a72cf24579b723eec5f7522e8a693d528
JaoVicy <filipimjoa@gmail.com>
2026-01-15 18:13:35 -0300

    fix: fixed var name

--- models/BubbleTea.go
+++ models/BubbleTea.go
@@ -1,6 +1,6 @@
 package models
 
-type BubleTea struct {
+type BubbleTea struct {
 	choices  []string         // items on the to-do list
 	cursor   int              // which to-do list item our cursor is pointing at
 	selected map[int]struct{} // which to-do items are selected
