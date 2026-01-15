ef8e8b782d8e64098c120b8262755ed99a8d823a
JaoVicy <filipimjoa@gmail.com>
2026-01-15 18:30:47 -0300

    feat: created Updated() func

--- models/BubbleTea.go
+++ models/BubbleTea.go
@@ -5,10 +5,52 @@ import tea "github.com/charmbracelet/bubbletea"
 type BubbleTea struct {
 	Choices  []string         // items on the to-do list
 	Cursor   int              // which to-do list item our cursor is pointing at
-	Selected map[int]struct{} // which to-do items are selected
+	Selected map[int]struct{} // which to-do items are Selected
 }
 
 func (m BubbleTea) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
 	return nil
+}
+
+func (m BubbleTea) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.Cursor > 0 {
+				m.Cursor--
+			}
+
+		// The "down" and "j" keys move the Cursor down
+		case "down", "j":
+			if m.Cursor < len(m.Choices)-1 {
+				m.Cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the Cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.Selected[m.Cursor]
+			if ok {
+				delete(m.Selected, m.Cursor)
+			} else {
+				m.Selected[m.Cursor] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
 }
